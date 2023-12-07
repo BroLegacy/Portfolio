593f5e61803fdea5b2fe3d84e4e5bd4cddd5f402
BroLegacy <corantyn.5@gmail.com>
2023-12-07 03:37:27 +0100

    [FOOTER] Fin footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,21 +4,23 @@
 
 <template>
   <footer>
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/projets">Projets</router-link>
-        </li>
-        <li>
-          <router-link to="/about">About</router-link>
-        </li>
-        <li>
-          <router-link to="/contact">Contact</router-link>
-        </li>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/projets">Projets</router-link>
+          </li>
+          <li>
+            <router-link to="/about">à-propos</router-link>
+          </li>
+          <li>
+            <router-link to="/contact">Contact</router-link>
+          </li>
+        </ul>
+      </nav>
+      <ul class="info">
+        <li class="pad">corantyn.5@email.com</li>
+        <li>06 04 51 01 13</li>
       </ul>
-    </nav>
+      <router-link to="/">Home</router-link>
   </footer>
 </template>
