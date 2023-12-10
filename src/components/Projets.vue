586af8e0246a93d57e7f489625285d2a6c9b853d
Corantyn VIGNON <corantyn.5@gmail.com>
2023-12-10 02:37:16 +0100

    [ALL] Fin de toutes les pages version desktop

--- src/components/Projets.vue
+++ src/components/Projets.vue
@@ -12,6 +12,57 @@ export default {
     </div>
   </div>
   <main class="content-projets">
-    <h1>Projets</h1>
+    <div class="parent">
+      <div class="div1 item">
+        <img src="../assets/img/bg.png">
+        <div class="text">
+          <span>Cuisine Organiser</span>
+          <p>Une application de recette</p>
+        </div>
+      </div>
+      <div class="div2 item">
+        <img src="../assets/img/bg.png">
+        <div class="text">
+          <span>Cuisine Organiser</span>
+          <p>Une application de recette</p>
+        </div>
+      </div>
+      <div class="div3 item">
+        <img src="../assets/img/bg.png">
+        <div class="text">
+          <span>Cuisine Organiser</span>
+          <p>Une application de recette</p>
+        </div>
+      </div>
+      <div class="div4 item">
+        <img src="../assets/img/bg.png">
+        <div class="text">
+          <span>Cuisine Organiser</span>
+          <p>Une application de recette</p>
+        </div>
+      </div>
+      <div class="div5 item">
+        <img src="../assets/img/bg.png">
+        <div class="text">
+          <span>Cuisine Organiser</span>
+          <p>Une application de recette</p>
+        </div>
+      </div>
+      <div class="div6 item">
+        <img src="../assets/img/bg.png">
+        <div class="text">
+          <span>Cuisine Organiser</span>
+          <p>Une application de recette</p>
+        </div>
+      </div>
+    </div>
+    <div class="btn">
+      <router-link to="/contact">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <path d="M13.4697 8.53033C13.1768 8.23744 13.1768 7.76256 13.4697 7.46967C13.7626 7.17678 14.2374 7.17678 14.5303 7.46967L18.5303 11.4697C18.8232 11.7626 18.8232 12.2374 18.5303 12.5303L14.5303 16.5303C14.2374 16.8232 13.7626 16.8232 13.4697 16.5303C13.1768 16.2374 13.1768 15.7626 13.4697 15.4697L16.1893 12.75H6.5C6.08579 12.75 5.75 12.4142 5.75 12C5.75 11.5858 6.08579 11.25 6.5 11.25H16.1893L13.4697 8.53033Z" fill="black"/>
+        </svg>
+        Contactez-moi
+      </router-link>
+    </div>
   </main>
 </template>
