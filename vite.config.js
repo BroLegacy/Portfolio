ba3eae4c121bab16e5f96df3b1a1b5e9916329d9
BroLegacy <corantyn.5@gmail.com>
2025-10-21 16:35:07 +0200

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    // Ajout pour forcer Vite à pré-compiler vue-meta,
+    // ce qui peut résoudre les erreurs "require is not defined".
+    include: ['vue-meta'],
+  },
 })
