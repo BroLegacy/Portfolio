1c490cf0a893ec39c1c1e8983d0493e5ad7122be
BroLegacy <corantyn.5@gmail.com>
2025-10-21 16:37:59 +0200

    Try Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,14 @@ export default defineConfig({
     // ce qui peut résoudre les erreurs "require is not defined".
     include: ['vue-meta'],
   },
+  build: {
+    // Configuration spécifique pour le build de production
+    commonjsOptions: {
+      // Cette option aide Rollup (le bundler de Vite) à gérer
+      // les modules qui mélangent les syntaxes CommonJS (require) et ES Modules (import).
+      // C'est souvent la clé pour résoudre les erreurs "require is not defined" sur Netlify.
+      transformMixedEsModules: true,
+    },
+  },
 })
+
